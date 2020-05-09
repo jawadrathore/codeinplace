2a636c6a8c1d99f6b698fa169752a1b8b394e54b
jawadrathore <61494044+jawadrathore@users.noreply.github.com>
2020-05-09 19:44:24 -0400

    Update forest_fire.py

--- forest_fire.py
+++ forest_fire.py
@@ -8,20 +8,6 @@ Those "sufficiently red" pixels are then highlighted in the
 image and the rest of the image is turned grey, by setting the
 pixels red, green, and blue values all to be the same average
 value.
-We’re going to start by writing a function called find_flames that
-highlights the areas where a forest fire is active. Detect all of the
-“sufficiently red” pixels in the image, which are indicative of where fires are burning in the image.
-When you detect a “sufficiently red” pixel in the original image, you set its red value to
-255 and its green and blue values to 0. This will highlight the pixel by making it entirely
-red. For all other pixels (i.e., those that are not “sufficiently red”), you should convert
-them to their grayscale equivalent, so that we can more easily see where the fire is
-originating from. You can grayscale a pixel by summing together its red, green, and blue
-values and dividing by three (finding the average), and then setting the pixel’s red, green,
-and blue values to all have this same “average” value.
-Once you highlight the areas that are on fire in the image (and greyscale all the remaining
-pixels), you should see an image like that shown on the right in the image bellow. On the
-left side of Figure 1, we should the original image for comparison.
-Original forest fire image on left, and highlighted version of image on right.
 """
 
 
